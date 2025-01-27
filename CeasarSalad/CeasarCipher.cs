c5d68154acdc664420c279ecf4599bdfc8347013
Riptidal7 <timdeu05@gmail.com>
2025-01-27 15:23:23 -0800

    added encode letter shifting

--- CeasarSalad/CeasarCipher.cs
+++ CeasarSalad/CeasarCipher.cs
@@ -4,15 +4,18 @@ public class CeasarCipher
 {
     public static string Encode(string message, int shift)
     {
+        string result = "";
         foreach (char c in message)
         {
-            
+            char shifted = (char)(c + shift);
+            result += shifted;
         }
-        
+        return result;
     }
 
     public static string Decode(string message, int shift)
     {
-        
+        string result = "";
+        return result;
     }
 }
--- CeasarSaladUnitTest/UnitTest1.cs
+++ CeasarSaladUnitTest/UnitTest1.cs
@@ -1,9 +1,13 @@
 namespace CeasarSaladUnitTest;
 
+using CeasarSalad;
+
 public class UnitTest1
 {
     [Theory]
-    [InlineData("Hello", 2, "Jgnnq")]
+    [InlineData("Hello", 3, "Khoor")]
+    [InlineData("Hello", -3, "Ebiil")]
+    [InlineData("Xyz", 3, "Abc")]
     public void Test1(string input, int shift, string encoded)
     {
         Assert.Equal(encoded, CeasarCipher.Encode(input, shift));
