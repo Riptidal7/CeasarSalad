ba4c7c5517e09578477d052bb2e986e1ef10df16
Riptidal7 <timdeu05@gmail.com>
2025-01-27 14:37:19 -0800

    added encode and decode methods

---
+++ CeasarSalad/CeasarCipher.cs
@@ -0,0 +1,18 @@
+﻿namespace CeasarSalad;
+
+public class CeasarCipher
+{
+    public static string Encode(string message, int shift)
+    {
+        foreach (char c in message)
+        {
+            
+        }
+        
+    }
+
+    public static string Decode(string message, int shift)
+    {
+        
+    }
+}
